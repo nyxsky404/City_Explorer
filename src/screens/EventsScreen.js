58df72155d2ef2ad78c505790513768ec28f23b0
Sumit Kumar <sumit76231@gmail.com>
2025-09-27 09:40:32 +0530

    Render Mock Data in Event Screen

--- src/screens/EventsScreen.js
+++ src/screens/EventsScreen.js
@@ -1,5 +1,35 @@
-const EventsScreen = () => {
-};
+import {View,Text,FlatList} from 'react-native';
+import { mockEvents } from '../data/eventsData';
 
+const EventsScreen = () => {
+
+  const renderEventData = ({ item }) => (
+    <View>
+      <View>
+        <Text>{item.title}</Text>
+      </View>
+      
+      <Text>{item.category}</Text>
+      <Text>{item.date} • {item.time}</Text>
+      <Text>📍 {item.location}</Text>
+      <Text numberOfLines={2}>
+        {item.description}
+      </Text>
+    </View>
+  );
+
+  return (
+    <View>
+      <View>
+        <Text>City Events</Text>
+        <Text>Discover what's happening in your city</Text>
+      </View>
+      
+      <FlatList data={mockEvents} renderItem={renderEventData}keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={true}
+      />
+    </View>
+  );
+};
 
 export default EventsScreen;
